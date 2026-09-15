a0327a91b45c9650489c887d23aa2798282177a4
Willie Shen <cg10250207@gmail.com>
2026-09-15 18:22:10 +0800

    2026-09-15T10:22:10+00:00

--- make.sh
+++ make.sh
@@ -4,5 +4,8 @@ rm -f Peskyfox.js user.js
 wget https://github.com/yokoffing/Betterfox/raw/refs/heads/main/Peskyfox.js
 wget https://raw.githubusercontent.com/arkenfox/user.js/refs/heads/Thorin-Oakenpants-patch-1/user.js
 test -f user.js || wget https://github.com/arkenfox/user.js/raw/refs/heads/master/user.js
-cat Peskyfox.js arkenfox-overrides.js Peskyfox-overrides.js extra-overrides.js >user-overrides.js
-cat user-overrides.js >>user.js
+cat Peskyfox.js arkenfox-overrides.js Peskyfox-overrides.js extra-overrides.js >>user.js
+wget https://raw.githubusercontent.com/arkenfox/user.js/refs/heads/Thorin-Oakenpants-patch-1/prefsCleaner.sh
+test -f prefsCleaner.sh || wget https://github.com/arkenfox/user.js/raw/refs/heads/master/prefsCleaner.sh
+wget https://raw.githubusercontent.com/arkenfox/user.js/refs/heads/Thorin-Oakenpants-patch-1/prefsCleaner.bat
+test -f prefsCleaner.bat || wget https://github.com/arkenfox/user.js/raw/refs/heads/master/prefsCleaner.bat
