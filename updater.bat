49979337661580b81186c8f7686283edca62a9e3
Willie Shen <cg10250207@gmail.com>
2026-09-15 19:54:15 +0800

    2026-09-15T11:54:15+00:00

--- updater.bat
+++ updater.bat
@@ -5,7 +5,7 @@ REM ## combinedfox user.js updater for Windows
 REM ## authors: @claustromaniac @Willie169
 REM ## version: 4.21
 REM ## credit: arkenfox user.js updater for Windows
-REM ## instructions: https://github.com/Willie169/combinedfox/wiki/2-Usage#Windows
+REM ## instructions: https://github.com/Willie169/combinedfox/wiki/2-combinedfox-user.js#updater
 
 SET v=4.21
 
